2b219f3a5597f36bff4b3ca960cc1750466a3f7e
Anatolii <volotovskiy111@gmail.com>
2022-05-08 12:03:24 +0300

    Добавление блока со слайдером

---
+++ src/js/script.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+    $('.carousel__inner').slick({
+        speed: 1300,
+        /*         adaptiveHeight: true, */
+        prevArrow: '<button type="button" class="slick-prev"><img src="../icon/left.svg"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="../icon/right.svg"></button>',
+        responsive: [{
+            breakpoint: 992,
+            settings: {
+                dots: true,
+                arrows: false
+            }
+        }]
+    });
+});
