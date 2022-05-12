0a4f3e03fd4308d91859943df1863658997d2564
Anatolii <volotovskiy111@gmail.com>
2022-05-12 18:15:43 +0300

    Добавление animation.css, валидация сайта

--- src/js/script.js
+++ src/js/script.js
@@ -100,4 +100,20 @@ $(document).ready(function() {
         });
         return false;
     });
+
+    new WOW().init();
+
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 1600) {
+            $('.pageup').fadeIn();
+        } else {
+            $('.pageup').fadeOut();
+        }
+    });
+
+    $("a[href=#up]").click(function() {
+        const _href = $(this).attr("href");
+        $("html, bodu").animate({ scrollTop: $(_href).offset().top + "px" });
+        return false;
+    });
 });
